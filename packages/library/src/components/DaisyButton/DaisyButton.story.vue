387b1e29d2e786e2196a9411a1c46237f4ae970e
JoseMa Santos <josantos@domingoalonsogroup.com>
2024-04-21 11:40:53 +0200

    ✨ add button

--- packages/library/src/components/DaisyButton/DaisyButton.story.vue
+++ packages/library/src/components/DaisyButton/DaisyButton.story.vue
@@ -1,44 +1,15 @@
-// DaisyButton.story.vue
 <script setup lang="ts">
 import { reactive } from 'vue'
 import DaisyButton from './DaisyButton.vue'
-import { sizes, Sizes, colorsBrand, colorsState } from '../../globals'
 
-const state = reactive({
-  content: 'Click Me',
-  disabled: false,
-  wide: false,
-  size: 'md' as Sizes,
-  circle: false,
-  square: false,
-  loading: false,
-  outline: false,
-  glass: false,
-  active: false
-})
-
-const colors = [...colorsBrand, ...colorsState, 'ghost', 'link'] as const
+const state = reactive({})
 </script>
 <template>
-  <Story :layout="{ type: 'grid', width: '50%' }">
-    <Variant title="No Color">
-      <DaisyButton v-bind="state">{{ state.content }}</DaisyButton>
-    </Variant>
-    <Variant v-for="color in colors" :key="color" :title="color">
-      <DaisyButton :color="color" v-bind="state">{{ state.content }}</DaisyButton>
+  <Story :layout="{ type: 'grid', width: '100%' }">
+    <Variant title="Variant Title">
+      <DaisyButton color="accent">Click me</DaisyButton>
     </Variant>
 
-    <template #controls>
-      <HstText v-model="state.content" title="Content" />
-      <HstCheckbox v-model="state.disabled" title="Disabled" />
-      <HstCheckbox v-model="state.active" title="Active" />
-      <HstCheckbox v-model="state.circle" title="Circle" />
-      <HstCheckbox v-model="state.square" title="Square" />
-      <HstCheckbox v-model="state.wide" title="Wide" />
-      <HstCheckbox v-model="state.loading" title="Loading" />
-      <HstCheckbox v-model="state.outline" title="Outline" />
-      <HstCheckbox v-model="state.glass" title="Glass" />
-      <HstSelect v-model="state.size" :options="sizes" title="Size" />
-    </template>
+    <template #controls></template>
   </Story>
 </template>
--- packages/library/src/components/DaisyButton/DaisyButton.vue
+++ packages/library/src/components/DaisyButton/DaisyButton.vue
@@ -1,65 +1,23 @@
-// DaisyButton.vue
 <script setup lang="ts">
+import { link } from 'fs'
 import { computed } from 'vue'
-import DaisyLoading from '../DaisyLoading/DaisyLoading.vue'
-import type { Sizes, ColorsBrand, ColorsState } from '../../globals'
 
-const props = withDefaults(
-  defineProps<{
-    color?: ColorsBrand | ColorsState | 'ghost' | 'link'
-    size?: Sizes
-    wide?: boolean
-    outline?: boolean
-    square?: boolean
-    circle?: boolean
-    loading?: boolean
-    glass?: boolean
-    active?: boolean
-  }>(),
-  {
-    size: 'md',
-    wide: false,
-    outline: false
-  }
-)
+const props = defineProps<{
+  color: 'primary' | 'secondary' | 'accent'
+}>()
 
-const colorClasses = computed(() => {
+const colorClass = computed(() => {
   return {
     neutral: 'btn-neutral',
     primary: 'btn-primary',
     secondary: 'btn-secondary',
     accent: 'btn-accent',
-    error: 'btn-error',
-    warning: 'btn-warning',
-    success: 'btn-success',
-    info: 'btn-info',
     ghost: 'btn-ghost',
     link: 'btn-link'
   }[props.color]
 })
-
-const sizeClasses = computed(() => {
-  return {
-    lg: 'btn-lg',
-    sm: 'btn-sm',
-    xs: 'btn-xs'
-  }[props.size]
-})
-
-const otherClasses = computed(() => {
-  return {
-    'btn-outline': props.outline,
-    'btn-wide': props.wide,
-    'btn-square': props.square,
-    'btn-circle': props.circle,
-    glass: props.glass,
-    'btn-active': props.active
-  }
-})
 </script>
+
 <template>
-  <button class="btn" :class="[colorClasses, sizeClasses, otherClasses]">
-    <DaisyLoading v-if="loading" :size="size" />
-    <slot></slot>
-  </button>
+  <button class="btn" :class="colorClass"><slot /></button>
 </template>
